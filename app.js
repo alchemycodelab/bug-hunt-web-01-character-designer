47dcfda412644cf54970e970f85f868e89a800d5
Danny Cairns <dpcairns@gmail.com>
2021-11-22 16:19:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// import functions and grab DOM elements
-
-// initialize state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 const headDropdown = document.getElementById('head-dropdown');
 const middleDropdown = document.getElementById('middle-dropdown');
 const bottomDropdown = document.getElementById('bottom-dropdown');
